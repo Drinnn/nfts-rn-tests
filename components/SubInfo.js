a929e65191c74a85f8cb2af3ae22c5ac963b72ec
Drinnn <pedro_lontro@hotmail.com>
2022-04-04 17:19:00 -0300

    feat: implement sub info component
    WIP: people and imageCmp complete

---
+++ components/SubInfo.js
@@ -0,0 +1,57 @@
+import { Image, Text, View } from "react-native";
+import { SIZES, assets } from "../constants";
+
+export const NFTTitle = () => {
+  return <Text>NFT Title</Text>;
+};
+
+export const EthPrice = () => {
+  return <Text>Eth Price</Text>;
+};
+
+export const ImageCmp = ({ imgUrl, index }) => {
+  return (
+    <Image
+      source={imgUrl}
+      resizeMode="contain"
+      style={{
+        width: 48,
+        height: 48,
+        marginLeft: index === 0 ? 0 : -SIZES.font,
+      }}
+    />
+  );
+};
+
+export const People = () => {
+  return (
+    <View style={{ flexDirection: "row" }}>
+      {[assets.person02, assets.person03, assets.person04].map(
+        (imgUrl, index) => (
+          <ImageCmp imgUrl={imgUrl} index={index} key={`People-${index}`} />
+        )
+      )}
+    </View>
+  );
+};
+
+export const EndDate = () => {
+  return <Text>EndDate</Text>;
+};
+
+export const SubInfo = () => {
+  return (
+    <View
+      style={{
+        width: "100%",
+        paddingHorizontal: SIZES.font,
+        marginTop: -SIZES.extraLarge,
+        flexDirection: "row",
+        justifyContent: "space-between",
+      }}
+    >
+      <People />
+      <EndDate />
+    </View>
+  );
+};
